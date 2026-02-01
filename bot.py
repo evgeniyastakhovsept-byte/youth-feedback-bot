c05f4aab039ddf0aa89ff419cafd03fa69293018
Evgeniy <evgeniy@MacBook-Air-Evgeniy.local>
2026-02-02 01:11:04 +0200

    Revert "Fix ConversationHandler per_message conflict"
    This reverts commit 536bd8236aa7a46f6794d5e20980019a6acc8882.

--- bot.py
+++ bot.py
@@ -1237,7 +1237,7 @@ def main():
             ],
         },
         fallbacks=[CommandHandler('start', start)],
-        per_message=False,
+        per_message=True,
     )
     
     # Регистрируем обработчики
